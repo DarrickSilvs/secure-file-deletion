3f1c473cfc82714529bc01aa5f6fcc8fbcfc34bc
Darrick <darricksilvester@gmail.com>
2025-07-21 01:35:20 +1000

    Added clap parser for file_path arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+pub struct Arguments {
+    // path to the file to shred
+    file_path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+    println!("path is: {:?}", args.file_path);
 }
